43983ccfac7631d089b6fa07f70b780bab4c0618
Priyanka Devaraj <devaraj.priyanka92@gmail.com>
2023-10-26 15:06:51 -0500

    Added here!

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function addTask() {
     var taskText = taskInput.value.trim();
 
     if (taskText === "") {
-        alert("Please enter a task here");
+        alert("Please enter a task here!");
         return;
     }
 
